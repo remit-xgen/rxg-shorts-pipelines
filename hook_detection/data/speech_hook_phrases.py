80798d9ad30d306d2d10c3b7f102f3e57e740d3d
remit-xgen <remitxgen@gmail.com>
2026-03-08 04:30:42 +0700

    Create speech_hook_phrases.py

---
+++ hook_detection/data/speech_hook_phrases.py
@@ -0,0 +1,21 @@
+"""
+Speech Hook Phrase Library
+Editable list of viral hook phrases
+"""
+
+HOOK_PHRASES = [
+
+    "you won't believe",
+    "wait for it",
+    "this changed everything",
+    "nobody talks about",
+    "here's the secret",
+    "watch this",
+    "this is crazy",
+    "i discovered",
+    "this is why",
+    "what happened next",
+    "this will shock you",
+    "the truth is",
+
+]
