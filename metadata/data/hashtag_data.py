39fc3c9a516901199304f38ec7dfbc41d90f87d7
remit-xgen <remitxgen@gmail.com>
2026-03-08 05:18:38 +0700

    Create hashtag_data.py

---
+++ metadata/data/hashtag_data.py
@@ -0,0 +1,34 @@
+"""
+Hashtag Dataset
+"""
+
+BASE_HASHTAGS = [
+    "shorts",
+    "viral",
+    "fyp",
+    "trending"
+]
+
+NICHE_HASHTAGS = {
+
+    "finance": [
+        "investing",
+        "money",
+        "finance",
+        "wealth"
+    ],
+
+    "motivation": [
+        "motivation",
+        "mindset",
+        "success",
+        "selfimprovement"
+    ],
+
+    "technology": [
+        "tech",
+        "ai",
+        "innovation",
+        "future"
+    ]
+}
