eadfb7a57525259dc794c4c70753a1eb82f8cd09
remit-xgen <remitxgen@gmail.com>
2026-03-08 05:08:51 +0700

    Create caption_templates.py

---
+++ metadata/data/caption_templates.py
@@ -0,0 +1,19 @@
+"""
+Caption Templates
+Collection of viral caption templates
+"""
+
+CAPTION_TEMPLATES = [
+
+    "You Won't Believe This About {topic}",
+    "This {topic} Trick Changes Everything",
+    "Nobody Talks About This {topic}",
+    "The Truth About {topic}",
+    "This Is Why {topic} Fails",
+    "Everyone Gets {topic} Wrong",
+    "The Biggest {topic} Mistake",
+    "This {topic} Secret Will Shock You",
+    "Stop Doing This With {topic}",
+    "This Is Why {topic} Matters"
+
+]
