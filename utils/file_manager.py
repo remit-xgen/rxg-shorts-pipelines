e7c6e2fa15f505617929c8b7e7f6bbef5cf3cebe
remit-xgen <remitxgen@gmail.com>
2026-03-08 03:55:44 +0700

    Create file_manager.py

---
+++ utils/file_manager.py
@@ -0,0 +1,67 @@
+"""
+File Manager Utility
+Handles folder creation, file paths, and cleanup
+"""
+
+import os
+import shutil
+from logging.logger import logger
+
+
+class FileManager:
+
+    @staticmethod
+    def ensure_dir(path):
+        """
+        Create folder if it doesn't exist
+        """
+        if not os.path.exists(path):
+            os.makedirs(path)
+            logger.info(f"Created directory: {path}")
+
+    @staticmethod
+    def file_exists(path):
+        """
+        Check if file exists
+        """
+        return os.path.isfile(path)
+
+    @staticmethod
+    def delete_file(path):
+        """
+        Delete file safely
+        """
+        if os.path.exists(path):
+            os.remove(path)
+            logger.info(f"Deleted file: {path}")
+
+    @staticmethod
+    def delete_folder(path):
+        """
+        Delete folder safely
+        """
+        if os.path.exists(path):
+            shutil.rmtree(path)
+            logger.info(f"Deleted folder: {path}")
+
+    @staticmethod
+    def list_files(folder):
+        """
+        List all files in folder
+        """
+        if not os.path.exists(folder):
+            return []
+
+        return [
+            os.path.join(folder, f)
+            for f in os.listdir(folder)
+            if os.path.isfile(os.path.join(folder, f))
+        ]
+
+    @staticmethod
+    def move_file(src, dst):
+        """
+        Move file
+        """
+        shutil.move(src, dst)
+        logger.info(f"Moved file {src} -> {dst}")
