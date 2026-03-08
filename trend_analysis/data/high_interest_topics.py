e6bf35593083ca7955742283de9c2c1db6541579
remit-xgen <remitxgen@gmail.com>
2026-03-08 08:10:48 +0700

    Create high_interest_topics.py

---
+++ trend_analysis/data/high_interest_topics.py
@@ -0,0 +1,26 @@
+"""
+High Interest Topics
+Topics with historically high engagement
+"""
+
+HIGH_INTEREST_TOPICS = [
+
+    "ai",
+    "money",
+    "business",
+    "crypto",
+    "tech",
+    "celebrity",
+    "news",
+    "war",
+    "politics",
+    "science",
+    "space",
+    "health",
+    "fitness",
+    "biohacking",
+    "startup",
+    "finance",
+    "investing"
+
+]
