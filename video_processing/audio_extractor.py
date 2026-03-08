386a28c0ff3e33592714e28e198bfaf981677a9a
remit-xgen <remitxgen@gmail.com>
2026-03-08 07:41:01 +0700

    Update audio_extractor.py

--- video_processing/audio_extractor.py
+++ video_processing/audio_extractor.py
@@ -6,13 +6,15 @@ Extract audio from video files
 import os
 from moviepy.editor import VideoFileClip
 from logging.logger import logger
-import config
 
 
 class AudioExtractor:
 
     def __init__(self):
-        os.makedirs(config.AUDIO_DIR, exist_ok=True)
+
+        self.temp_dir = "temp"
+        os.makedirs(self.temp_dir, exist_ok=True)
+
 
     def extract(self, video_path):
 
@@ -25,14 +27,20 @@ class AudioExtractor:
             video_name = os.path.basename(video_path).split(".")[0]
 
             audio_path = os.path.join(
-                config.AUDIO_DIR,
+                self.temp_dir,
                 f"{video_name}.wav"
             )
 
             logger.info(f"Extracting audio from {video_path}")
 
             video = VideoFileClip(video_path)
-            video.audio.write_audiofile(audio_path)
+
+            video.audio.write_audiofile(
+                audio_path,
+                logger=None
+            )
+
+            video.close()
 
             logger.info(f"Audio saved: {audio_path}")
 
