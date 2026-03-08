198e28a2f8d94800bff2343fc8174f3f18ebec5d
remit-xgen <remitxgen@gmail.com>
2026-03-08 08:10:20 +0700

    Create data/viral_triggers.py

---
+++ trend_analysis/data/viral_triggers.py
@@ -0,0 +1,24 @@
+"""
+Viral Trigger Words
+Words strongly correlated with viral content
+"""
+
+VIRAL_TRIGGERS = [
+
+    "secret",
+    "truth",
+    "hack",
+    "shocking",
+    "leaked",
+    "revealed",
+    "exposed",
+    "before it's deleted",
+    "viral",
+    "insane",
+    "crazy",
+    "warning",
+    "don't watch",
+    "nobody knows",
+    "this changes everything"
+
+]
